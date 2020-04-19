2cdc414e02c940071128c7ea276167b923f6a439
KostadinovK <kostadinovk@gmail.com>
2020-04-19 11:35:58 +0300

    Added footer component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <Router>
       <Header/>
+      <Footer/>
     </Router>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Grid &copy; 2020 - All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
