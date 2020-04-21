3f0faaaefbc671df2b0ded8dd466231841ee2fbf
KostadinovK <kostadinovk@gmail.com>
2020-04-21 10:24:29 +0300

    Added register functionality

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,35 +1,80 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import '../../shared/styles/forms.css';
 
 import { UserContext } from '../App/App';
 
+import userService from '../../services/userService';
+
 function Register() {
 
     const history = useHistory();
     let {isLoggedIn} = useContext(UserContext);
+
+    let [form, setForm] = useState({username: '', pass: '', rePass: ''});
+    let [error, setError] = useState(null);
 
     if(isLoggedIn){
         history.push('/');
         return null;
     }
 
+    const onInputChange = (event) => {
+        const name = event.target.id;
+        const value = event.target.value;
+
+        setForm((form) => {
+            return { ...form, [name]: value } ;
+        });
+    };
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        const {username, pass, rePass} = form;
+        
+        if(username.length < 3){
+            setError('Username length must be atleast 3 symbols!');
+            return;
+        }
+
+        if(pass !== rePass){
+            setError('Passwords must match!');
+            return;
+        }
+
+        if(pass.length < 3){
+            setError('Password length must be atleast 3 symbols!');
+            return;
+        }
+
+        let data = await userService.register(username, pass);
+
+        if(data.error){
+            setError(data.error);
+            return;
+        }
+        
+        history.push('/login');
+    };
+
     return (
 
-        <form className="register-form">
+        <form className="register-form" onSubmit={(e) => register(e)}>
             <h2>Register</h2>
+            {error ? <p className='error'>{error}</p> : null}
             <p className="username">
-                <label htmlFor="username"><i class="fas fa-user"></i></label>
-                <input type="text" name="username" id='username' placeholder="Username" />
+                <label htmlFor="username"><i className="fas fa-user"></i></label>
+                <input type="text" name="username" id='username' placeholder="Username" onChange={(e) => onInputChange(e)}/>
             </p>
             <p className="password">
-                <label htmlFor="pass"><i class="fas fa-key"></i></label>
-                <input type="password" name="password" id='pass' placeholder="Password" />
+                <label htmlFor="pass"><i className="fas fa-key"></i></label>
+                <input type="password" name="password" id='pass' placeholder="Password" onChange={(e) => onInputChange(e)}/>
             </p>
             <p className="confirm-password">
-                <label htmlFor="rePass"><i class="fas fa-key"></i></label>
-                <input type="password" name="confirmPassword" id='rePass' placeholder="Confirm Password" />
+                <label htmlFor="rePass"><i className="fas fa-key"></i></label>
+                <input type="password" name="confirmPassword" id='rePass' placeholder="Confirm Password" onChange={(e) => onInputChange(e)}/>
             </p>
             <input className='submitButton' type="submit" value="Register" />
         </form>
---
+++ client/src/services/userService.js
@@ -0,0 +1,15 @@
+const baseUrl = 'http://localhost:5000/api/user';
+
+function register(username, password){
+    const url = baseUrl + '/register';
+    
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    }).then(resp => resp.json());
+}
+
+export default {register};
